fc942ea01945d636fff368388b8adf32ed949f42
suriya_stark <136789008+SuriyaPasupathi@users.noreply.github.com>
2025-03-31 11:31:15 +0530

    Update vite.config.js

--- myapp/vite.config.js
+++ myapp/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true,          // Exposes to local network (e.g., 192.168.x.x)
+    port: 5173,          // Optional: specify port
+  },
+});
